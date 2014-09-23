86f8cb51d5194d22c30d930ad8643c1a5d319aec
Yassine Guedidi <yassine@guedidi.com>
2014-09-23 11:55:45 +0200

    Add thumbnail upscale

--- web/app/plugins/tvjussieu/tvjussieu.php
+++ web/app/plugins/tvjussieu/tvjussieu.php
@@ -79,3 +79,19 @@ if ( class_exists( 'TVJussieu' ) ) {
 
 	$tvj = new TVJussieu();
 }
+
+add_filter('image_resize_dimensions', function ($default, $orig_w, $orig_h, $new_w, $new_h, $crop) {
+	if(!$crop)
+		return null; // let the wordpress default function handle this
+
+	$aspect_ratio = $orig_w / $orig_h;
+	$size_ratio = max($new_w / $orig_w, $new_h / $orig_h);
+
+	$crop_w = round($new_w / $size_ratio);
+	$crop_h = round($new_h / $size_ratio);
+
+	$s_x = floor( ($orig_w - $crop_w) / 2 );
+	$s_y = floor( ($orig_h - $crop_h) / 2 );
+
+	return array( 0, 0, (int) $s_x, (int) $s_y, (int) $new_w, (int) $new_h, (int) $crop_w, (int) $crop_h );
+}, 10, 6);

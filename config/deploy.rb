4e0846a857d1fa487a2ef28de1aaea5ee4ef4bcc
Yassine Guedidi <yassine@guedidi.com>
2014-09-23 15:36:29 +0200

    Configure Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,34 +1,23 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'TV Jussieu'
+set :scm, :copy
 
-# Branch options
-# Prompts for the branch name (defaults to current branch)
-#ask :branch, -> { `git rev-parse --abbrev-ref HEAD`.chomp }
-
-# Hardcodes branch to always be master
-# This could be overridden in a stage config file
-set :branch, :master
-
-set :deploy_to, -> { "/srv/www/#{fetch(:application)}" }
+server 'ftp.tvjussieu.com', user: 'tvjussie', port: 22, password: 'ueissujvt14', roles: %w{web app db}
 
 set :log_level, :info
 
-# Apache users with .htaccess files:
-# it needs to be added to linked_files so it persists across deploys:
-# set :linked_files, %w{.env web/.htaccess}
-set :linked_files, %w{.env}
+set :linked_files, %w{.env web/.htaccess}
 set :linked_dirs, %w{web/app/uploads}
 
-namespace :deploy do
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :service, :nginx, :reload
-    end
+set :exclude_dir, [".env", "web/.htaccess", "web/app/uploads"]
+
+before "deploy:starting", :composer_for_prod do
+  run_locally do
+    execute "composer update --ansi --no-interaction --no-dev"
   end
 end
 
-# The above restart task is not run by default
-# Uncomment the following line to run it on deploys if needed
-# after 'deploy:publishing', 'deploy:restart'
+after "deploy:finished", :composer_for_dev do
+  run_locally do
+    execute "composer update --ansi --no-interaction --dev"
+  end
+end

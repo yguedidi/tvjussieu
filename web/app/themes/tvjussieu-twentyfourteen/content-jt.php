38b88dee2aa758bdaa268f99e747b0f1df814777
Yassine Guedidi <yassine@guedidi.com>
2014-10-03 11:27:44 +0200

    New JT style

--- web/app/themes/tvjussieu-twentyfourteen/content-jt.php
+++ web/app/themes/tvjussieu-twentyfourteen/content-jt.php
@@ -11,15 +11,41 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php if (is_front_page() ) : $title = get_the_title(); ?>
 	<header class="entry-header">
-		<?php if ( is_front_page() ) : $title = get_the_title(); ?>
-		<h1 class="entry-title">Dernier JT<?php echo $title ? ' : ' . $title : ''; ?></h1>
-		<?php else : ?>
-			<?php if ( in_array( 'category', get_object_taxonomies( get_post_type() ) ) && twentyfourteen_categorized_blog() ) : ?>
-				<div class="entry-meta">
-					<span class="cat-links"><?php echo get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'twentyfourteen' ) ); ?></span>
-				</div>
-				<?php
+		<h1 class="entry-title">
+			<a href="<?= esc_url( get_permalink() ); ?>" rel="bookmark">Dernier JT<?php echo $title ? ' : ' . $title : ''; ?></a>
+		</h1>
+	</header>
+	<?php endif; ?>
+
+	<?php
+	if ( is_singular() && $video = get_post_meta( get_the_ID(), 'jt_video', true ) ) :
+	?>
+	<div class="post-thumbnail">
+	<?php
+		echo wp_oembed_get( $video, array('width'=>672) );
+	?>
+	</div>
+	<?php else : ?>
+	<a class="post-thumbnail" href="<?php the_permalink(); ?>">
+	<?php
+		if ( ( ! is_active_sidebar( 'sidebar-2' ) || is_page_template( 'page-templates/full-width.php' ) ) ) {
+			the_post_thumbnail( 'twentyfourteen-full-width' );
+		} else {
+			the_post_thumbnail();
+		}
+	?>
+	</a>
+	<?php endif; ?>
+
+	<?php if ( ! is_front_page() ) : ?>
+	<header class="entry-header">
+		<?php if ( in_array( 'category', get_object_taxonomies( get_post_type() ) ) && twentyfourteen_categorized_blog() ) : ?>
+		<div class="entry-meta">
+			<span class="cat-links"><?php echo get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'twentyfourteen' ) ); ?></span>
+		</div>
+		<?php
 			endif;
 
 			if ( is_single() ) :
@@ -27,57 +53,44 @@
 			else :
 				the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
 			endif;
-			?>
+		?>
 
-			<div class="entry-meta">
-				<?php
+		<div class="entry-meta">
+			<?php
 				if ( 'post' == get_post_type() )
 					twentyfourteen_posted_on();
 
-				if ( !post_password_required() && ( comments_open() || get_comments_number() ) ) :
-					?>
-					<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyfourteen' ), __( '1 Comment', 'twentyfourteen' ), __( '% Comments', 'twentyfourteen' ) ); ?></span>
-					<?php
+				if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) :
+			?>
+			<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyfourteen' ), __( '1 Comment', 'twentyfourteen' ), __( '% Comments', 'twentyfourteen' ) ); ?></span>
+			<?php
 				endif;
 
 				edit_post_link( __( 'Edit', 'twentyfourteen' ), '<span class="edit-link">', '</span>' );
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
+			?>
+		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
-	<div class="entry-content">
-	<?php if ( is_singular() ) :?>
-		<?php
-		if ( $video = get_post_meta( get_the_ID(), 'jt_video', true ) ) :
-			echo wp_oembed_get( $video );
-		endif;
-		?>
-	<?php else : ?>
-		<a href="<?php the_permalink(); ?>">
-		<?php the_post_thumbnail(); ?>
-		</a>
-	<?php endif; // End is_singular() ?>
-	</div>
 
-	<?php if ( is_front_page() ) : ?>
-	<?php elseif ( is_search() || is_front_page() || is_archive() ) : ?>
-		<div class="entry-summary">
-			<?php the_excerpt(); ?>
-		</div><!-- .entry-summary -->
+	<?php if ( ! is_single() ) : ?>
+	<div class="entry-summary">
+		<?php the_excerpt(); ?>
+	</div><!-- .entry-summary -->
 	<?php else : ?>
-		<div class="entry-content">
-			<?php echo do_shortcode( '[facebook_like_button]' ); ?><br/><br/>
-			<?php
+	<div class="entry-content">
+		<?php echo do_shortcode( '[facebook_like_button]' ); ?><br/><br/>
+		<?php
 			the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyfourteen' ) );
 			wp_link_pages( array(
-				'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfourteen' ) . '</span>',
-				'after' => '</div>',
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfourteen' ) . '</span>',
+				'after'       => '</div>',
 				'link_before' => '<span>',
-				'link_after' => '</span>',
+				'link_after'  => '</span>',
 			) );
-			?>
-		</div><!-- .entry-content -->
+		?>
+	</div><!-- .entry-content -->
 	<?php endif; ?>
 
 	<?php the_tags( '<footer class="entry-meta"><span class="tag-links">', '', '</span></footer>' ); ?>
+
+	<?php endif; ?>
 </article><!-- #post-## -->
